72325292218c3ae7998a000907e7d702ab281964
ruanyf <ruanyf@users.noreply.github.com>
2025-10-04 14:54:42 +0000

    deploy: 8ae634d5203adaad990e12023fa382ff84cdc07b

---
+++ page.js
@@ -0,0 +1,29 @@
+fetch('./rss.json')
+.then(async function (response) {
+  const res = await response.json();
+  const items = res.items;
+
+  const list = document.querySelector('.list');
+
+  items.forEach(i => {
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    const host = (new URL(i.url)).hostname;
+    p.innerHTML = `<a href="${i.url}" target="_blank">${i.title}</a>（${timeStr(i.date_modified)}）<br><br>${i.content_html}`;
+    li.appendChild(p);
+    list.appendChild(li);
+  });
+})
+
+function timeStr(dateStr) {
+  try {
+    const d = new Date(dateStr);
+    return d.toLocaleString('zh-CN', {
+      dateStyle: 'short',
+      timeStyle: 'short',
+      hour12: false
+    });
+  } catch(e) {
+    return '';
+  }
+}
